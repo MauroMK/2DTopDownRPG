7ec762cb34ee002665c7cddf6496da55a30d3dcd
Mauro Moreno <mmauromoreno1@gmail.com>
2022-09-16 15:59:08 -0300

    Floating Text - Finished the floating text script

--- 2DTopDownRPG/Assets/Scripts/FloatingTextManager.cs
+++ 2DTopDownRPG/Assets/Scripts/FloatingTextManager.cs
@@ -10,9 +10,28 @@ public class FloatingTextManager : MonoBehaviour
 
     private List<FloatingText> floatingTexts = new List<FloatingText>();
 
+    private void Update()
+    {
+        foreach (FloatingText txt in floatingTexts)
+        {
+            txt.UpdateFloatingText();
+        }
+    }
+
     private void Show(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
     {
-        
+        FloatingText floatingText = GetFloatingText();
+
+        floatingText.txt.text = msg;
+        floatingText.txt.fontSize = fontSize;
+        floatingText.txt.color = color;
+
+        floatingText.gObject.transform.position = Camera.main.WorldToScreenPoint(position); // Transfer world space to screen space so we can use it in the UI
+        floatingText.motion = motion;
+        floatingText.duration = duration;
+
+        floatingText.Show();
+
     }
 
     private FloatingText GetFloatingText()
